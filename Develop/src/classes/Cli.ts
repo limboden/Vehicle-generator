4293a077a457ca1d36179f6e582d064c2711c960
limboden <imbodenlu@gmail.com>
2024-11-24 12:18:16 -0500

    perfoemed actions on the truck

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -201,6 +201,8 @@ class Cli {
         // TODO: set the selectedVehicleVin to the vin of the truck
         this.selectedVehicleVin = truck.vin;
         // TODO: perform actions on the truck
+        this.performActions();
+
 
       });
   }
