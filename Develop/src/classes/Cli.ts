5565fb12ef2e4c867e93171f16ff7e974e330b0a
limboden <imbodenlu@gmail.com>
2024-11-24 13:00:38 -0500

    add statements to perform the wheelie action only if the selected vehicle is a motorbike

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -402,6 +402,19 @@ class Cli {
           }
         }
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
+        else if (answers.action === 'Wheelie') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+              if (this.vehicles[i] instanceof Motorbike) {
+                const bike = this.vehicles[i] as Motorbike
+                bike.wheelie();
+                return;
+              }
+
+            }
+          }
+        }
+
         else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
